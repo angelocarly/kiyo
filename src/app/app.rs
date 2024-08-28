50db4ef7c315571a80cb02250f90a1ca39312573
CIOSAI <jyinteractive.tw@gmail.com>
2024-08-28 16:40:15 +0800

    integrated cpal audio output

--- src/app/app.rs
+++ src/app/app.rs
@@ -10,8 +10,9 @@ use notify::event::AccessMode::Write;
 use winit::event::{Event, StartCause, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::platform::run_on_demand::EventLoopExtRunOnDemand;
+use cpal::traits::StreamTrait;
 use crate::app::draw_orch::DrawConfig;
-use crate::app::{DrawOrchestrator, Renderer, Window};
+use crate::app::{DrawOrchestrator, Renderer, Window, StreamFactory};
 
 // Maybe delete all the following blocks
 use crate::vulkan::{Device, RenderPass, Framebuffer, CommandBuffer};
@@ -85,7 +86,7 @@ impl App {
         }
     }
 
-    pub fn run(mut self, draw_config: DrawConfig) {
+    pub fn run(mut self, draw_config: DrawConfig, audio_shader: Option<fn(f32)->(f32, f32)>) {
 
         let resolution = UVec2::new( self.window.get_extent().width, self.window.get_extent().height );
         let mut orchestrator = match DrawOrchestrator::new(&mut self.renderer, resolution, &draw_config) {
@@ -106,6 +107,58 @@ impl App {
         for path in paths {
             watcher.watch(Path::new(path), RecursiveMode::Recursive).unwrap();
         };
+
+        // audio
+
+        fn silence(_:f32) -> (f32, f32) {(0.0, 0.0)}
+        
+        let sf = StreamFactory::default_factory().unwrap();
+
+        let sample_rate = sf.config().sample_rate.0;
+        let mut sample_clock = 0;
+        let routin = move |len: usize| -> Vec<f32> {
+            (0..len / 2) // len is apparently left *and* right
+                .flat_map(|_| {
+                    sample_clock = (sample_clock + 1) % sample_rate;
+
+                    let (l, r) = audio_shader.unwrap_or(silence)(sample_clock as f32 / sample_rate as f32);
+                    vec![l, r]
+                })
+                .collect()
+        };
+        
+        let stream = sf.create_stream(routin).unwrap(); // creates stream from function "routin"
+        StreamTrait::play(&stream).unwrap();
+
+        /* For some reason if it's possible to branch, it doesn't play any sound
+
+        let mut play_audio = true;
+        match audio_shader {
+            Some(_) => {play_audio = true},
+            None => {println!("no audio");}
+        }
+        
+        if (play_audio){
+            let sf = StreamFactory::default_factory().unwrap();
+    
+            let sample_rate = sf.config().sample_rate.0;
+            let mut sample_clock = 0;
+            let routin = move |len: usize| -> Vec<f32> {
+                (0..len / 2) // len is apparently left *and* right
+                    .flat_map(|_| {
+                        sample_clock = (sample_clock + 1) % sample_rate;
+    
+                        let (l, r) = audio_shader.unwrap()(sample_clock as f32 / sample_rate as f32);
+                        vec![l, r]
+                    })
+                    .collect()
+            };
+            
+            let stream = sf.create_stream(routin).unwrap();
+            StreamTrait::play(&stream).unwrap();
+        } */
+        
+        // audio
 
         let mut last_print_time = SystemTime::now();
         let mut frame_count = 0;
